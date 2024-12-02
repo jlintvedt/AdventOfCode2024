4d271ee028f2342a3c34ae8339bf1a7fc9ed6a2b
jlintvedt <jlintvedt@gmail.com>
2024-12-02 06:18:17 +0100

    solve 02-1

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,66 @@ namespace AdventOfCode
     /// </summary>
     public class Day02
     {
+        public class RednoseReports
+        {
+            public List<Report> Reports = [];
+
+            public RednoseReports(string input)
+            {
+                foreach (var line in input.Split(Environment.NewLine))
+                {
+                    Reports.Add(new Report(line));
+                }
+            }
+
+            public int FindNumSafeReport()
+            {
+                var numSafe = 0;
+
+                foreach (var report in Reports)
+                    if (report.IsGraduallyChanging())
+                        numSafe++;
+
+                return numSafe;
+            }
+
+            public class Report
+            {
+                public List<int> Levels = [];
+
+                public Report(string line)
+                {
+                    foreach (var num in line.Split(" "))
+                    {
+                        Levels.Add(int.Parse(num));
+                    }
+                }
+
+                public bool IsGraduallyChanging()
+                {
+                    var prev = Levels[0];
+                    var isIncreasing = prev - Levels[1] < 0 ? true : false;
+
+                    for (int i = 1; i < Levels.Count; i++)
+                    {
+                        var diff = isIncreasing ? Levels[i] - prev : prev - Levels[i];
+
+                        if (diff < 1 || diff > 3)
+                            return false;
+
+                        prev = Levels[i];
+                    }
+
+                    return true;
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var rr = new RednoseReports(input);
+            return rr.FindNumSafeReport().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Run without debugger: `ctrl+f5` in VS Code. This stores the benchmark in [result
 
 ## Runtimes
 <!--ResultTableStart-->
+|                                |         | Test @4.5GHz<sup>1</sup> | Benchmark<sup>2</sup> |
+|--------------------------------|---------|-------------------------:|----------------------:|
+| [Day01](AdventOfCode/Day01.cs) | Puzzle1 |                     <1ms |                  67μs |
+|                                | Puzzle2 |                     <1ms |                  77μs |
 <!--ResultTableEnd-->
 
 1) AMD Ryzen 7 7700X @ 4.5GHz. Visual Studio Test Explorer
