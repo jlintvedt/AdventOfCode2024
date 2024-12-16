22b81b8f960e764c3e4599fd1f95248ad619adff
jlintvedt <jlintvedt@gmail.com>
2024-12-16 13:07:02 +0100

    solve 05-1

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,110 @@ namespace AdventOfCode
     /// </summary>
     public class Day05
     {
+        public class PrintingQueue
+        {
+            private readonly Dictionary<int, PageRules> rules = [];
+            private readonly List<SafetyManual> safetyManuals = [];
+
+            public PrintingQueue(string input)
+            {
+                var parts = input.Split($"{Environment.NewLine}{Environment.NewLine}");
+
+                foreach (var rule in parts[0].Split(Environment.NewLine))
+                    AddPageRule(rule);
+
+                foreach (var sequence in parts[1].Split(Environment.NewLine))
+                    safetyManuals.Add(new SafetyManual(sequence));
+            }
+
+            public int FindSumOfMiddlePageForValidManuals()
+            {
+                var sum = 0;
+
+                foreach (var manual in safetyManuals)
+                    if (manual.IsValidPageOrder(rules))
+                        sum += manual.GetMiddlePage();
+
+                return sum;
+            }
+
+            private void AddPageRule(string ordering)
+            {
+                var parts = ordering.Split('|');
+                var page1 = int.Parse(parts[0]);
+                var page2 = int.Parse(parts[1]);
+
+                var rule = GetPageRules(page1);
+                rule.AddPageAfterRule(page2);
+
+                // Call with the other page number as well to make sure a rule is created for every page.
+                GetPageRules(page2);
+            }
+
+            private PageRules GetPageRules(int pageNum)
+            {
+                if(!rules.TryGetValue(pageNum, out PageRules rule))
+                {
+                    rule = new PageRules(pageNum);
+                    rules.Add(pageNum, rule);
+                }
+
+                return rule;
+
+            }
+
+            public class PageRules(int pageNumber)
+            {
+                public int PageNumber = pageNumber;
+
+                public HashSet<int> AllowedPagesAfter = [];
+
+                public void AddPageAfterRule(int pageNumber)
+                {
+                    AllowedPagesAfter.Add(pageNumber);
+                }
+
+                public bool IsValid(List<int> pagesBefore)
+                {
+                    foreach (var page in pagesBefore)
+                        if (AllowedPagesAfter.Contains(page))
+                            return false;
+
+                    return true;
+                }
+            }
+
+            public class SafetyManual
+            {
+                public List<int> PageNumbers = [];
+
+                public SafetyManual(string rawInput)
+                {
+                    foreach (var num in rawInput.Split(','))
+                        PageNumbers.Add(int.Parse(num));
+                }
+
+                public bool IsValidPageOrder(Dictionary<int, PageRules> rules)
+                {
+                    for (int i = 1; i < PageNumbers.Count; i++)
+                        if (!rules[PageNumbers[i]].IsValid(PageNumbers[0..(i)]))
+                            return false;
+
+                    return true;
+                }
+
+                public int GetMiddlePage()
+                {
+                    return PageNumbers[PageNumbers.Count / 2];
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var pq = new PrintingQueue(input);
+            return pq.FindSumOfMiddlePageForValidManuals().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day05Tests.cs
+++ AdventOfCodeTests/Day05Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day05Tests
     {
@@ -24,7 +23,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day05.Puzzle1(InputProvider.GetExample(AdventOfCode.Const.Year, day, exampleNum: 1));
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"143", result);
         }
 
         [TestMethod]
@@ -34,7 +33,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day05.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"5374", result);
         }
 
         [TestMethod]
